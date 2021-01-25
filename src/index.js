f0f3ef51f42199b0299364af655d6e38381118e2
Javier Aragón <javier.aragon@geographica.gs>
2021-01-25 18:21:42 +0100

    bug fixed

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import init from './init';
 
 const element = document.getElementById('root');
 
-const api_key = 'default_public';
-const props = {
-  username: 'alasarr',
-  type: 'bigquery',
-  query: new URLSearchParams(`?data=cartobq.maps.osm_buildings&api_key=${api_key}`),
-  shareOptions: {
-    baseUrl: 'https://viewer.carto.com/',
-  },
-};
+// const api_key = 'default_public';
+// const props = {
+//   username: 'alasarr',
+//   type: 'bigquery',
+//   query: new URLSearchParams(`?data=cartobq.maps.osm_buildings&api_key=${api_key}`),
+//   shareOptions: {
+//     baseUrl: 'https://viewer.carto.com/'
+//   }
+// };
 
-init(element, props);
+// init(element, props);
 
-// init(element);
+init(element);
